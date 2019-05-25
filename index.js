52a6d3e9d0e52036b752f89f6c206b23f1afc74b
Hüseyin Yiğit <yigit.hsyn@gmail.com>
2019-05-25 23:05:38 +0000

    calculation with change stream tested

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const args = argParser.parseArgs()
 //-------------------------------------
 let mongodb;
 const mongoClient = require("mongodb").MongoClient
-const mongoObjectId = require("mongodb").ObjectID
+const mongoChangeStrem = require("mongodb").ChangeStream
 const mongodbUrl = "mongodb://127.0.0.1:27017"
 mongoClient.connect(mongodbUrl, { poolSize: 10, useNewUrlParser: true }, function (err, client) {
   assert.equal(null, err);
@@ -110,8 +110,8 @@ app.use(require("cors")())
 const {pathLoss, radioHorizon} = require('@softroles/propagation')
 
 app.get('/calculator/api/v1/propagation/pathloss', function (req, res) {
-  const freq = parseInt(req.query.freq) || 0 
-  const dist = parseInt(req.query.dist) || 0
+  const freq = parseFloat(req.query.freq) || 0 
+  const dist = parseFloat(req.query.dist) || 0
   //console.log(freq)
   //console.log(dist)
   //console.log(parseInt(String(pathLoss(freq,dist))))
@@ -119,17 +119,29 @@ app.get('/calculator/api/v1/propagation/pathloss', function (req, res) {
 });
 
 app.get('/calculator/api/v1/propagation/radiohorizon', function (req, res) {
-  const h = parseInt(req.query.h) || 0 
+  const h = parseFloat(req.query.h) || 0 
   res.send(''+radioHorizon(h))
 });
 app.get('/calculator/api/:module/:function', function (req, res) {
-  console.log(req.params.module)
-  console.log(req.params.function)
-  console.log(req.query)
+  //console.log(req.params.module)
+  //console.log(req.params.function)
+  //console.log(req.query)
   req.query = {function: req.params.function, args: req.query}
   mongodb.db("modules").collection(req.params.module).insertOne(req.query, function (err, r) {
     if (err) res.send({ error: err })
-    else res.send(r.insertedId)
+    else {
+      const changeStream =  mongodb.db("modules").collection(req.params.module).watch({ fullDocument: 'updateLookup' })
+      changeStream.on('change', event => {
+        //console.log(event)
+        if(event.operationType == 'update'){
+          //console.log(event.fullDocument)
+          //console.log(event.fullDocument.output)
+          res.send(String(event.fullDocument.output))
+          changeStream.close()
+        }
+      })
+    
+    }//res.send(r.insertedId)
   });
 });
 //=============================================================================
